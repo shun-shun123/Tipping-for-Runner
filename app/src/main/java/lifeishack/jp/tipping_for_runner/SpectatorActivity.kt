c5b21b591bc4ac5aabbff6c9e0cbb1b53e486781
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-11-02 00:15:18 +0900

    SpectatorActivity作成

---
+++ app/src/main/java/lifeishack/jp/tipping_for_runner/SpectatorActivity.kt
@@ -0,0 +1,12 @@
+package lifeishack.jp.tipping_for_runner
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SpectatorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spectator)
+    }
+}
